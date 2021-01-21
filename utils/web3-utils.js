1dc943fd74d0d6132c1b7af2bc4ef92d3c4c3c84
Luciano Bertenasco <lbertenasco@gmail.com>
2021-01-21 12:18:06 -0300

    feat: keep3r escrow (#7)
    * feat: keep3r escrow
    
    * fix: approval and naming
    
    * feat: escrow scripts
    
    * fix: comments and escrow addresses
    
    * feat: 2 escrows deployed

--- utils/web3-utils.js
+++ utils/web3-utils.js
@@ -5,11 +5,13 @@ const gwei = ethers.BigNumber.from(10).pow(9);
 const e18 = ethers.BigNumber.from(10).pow(18);
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 const SIX_HOURS = 6 * 60 * 60;
-
+const P = 100000;
+const e18ToDecimal = (number) => number.mul(P).div(e18).toNumber() / P;
 
 module.exports = {
     gwei,
     e18,
     ZERO_ADDRESS,
     SIX_HOURS,
+    e18ToDecimal,
 }
