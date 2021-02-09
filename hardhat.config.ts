4b2f3dc4abdbca8b01eec62448ff731bee977f32
b0dhidharma <b0dhidharma@protonmail.com>
2021-02-09 17:11:52 -0300

    migrating one script as example

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,38 +1,29 @@
 import '@nomiclabs/hardhat-waffle';
-import '@nomiclabs/hardhat-ganache';
 import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-solhint';
 import { removeConsoleLog } from 'hardhat-preprocessor';
 import 'hardhat-gas-reporter';
 import 'solidity-coverage';
 import config from './.config.json';
-
-let hardhat = {};
-
-if (process.env.FORK) { 
-  hardhat = {
-    forking: { 
-      url: `https://eth-mainnet.alchemyapi.io/v2/${config.alchemy.mainnet.apiKey}`
-    }
-  };
-}
+import { utils } from 'ethers';
 
 module.exports = {
   defaultNetwork: 'hardhat',
   networks: {
-    hardhat,
+    hardhat: {
+      enabled: (process.env.FORK) ? true : false,
+      accounts: [{
+        privateKey: config.accounts.mainnet.privateKey,
+        balance: utils.parseEther('1000').toString()
+      }],
+      forking: { 
+        url: `https://eth-mainnet.alchemyapi.io/v2/${config.alchemy.mainnet.apiKey}`
+      }
+    },
     localMainnet: {
       url: 'http://127.0.0.1:8545',
       accounts: [config.accounts.mainnet.privateKey],
       gasMultiplier: 1.1,
-    },
-    ganache: {
-      url: "http://127.0.0.1:8545",
-      gasLimit: 6000000000,
-      defaultBalanceEther: 100,
-      fork: `https://mainnet.infura.io/v3/${config.infura.apiKey}`,
-      unlocked_accounts: ['0x1ea056C13F8ccC981E51c5f1CDF87476666D0A74'],
-      keepAliveTimeout: 100000,
     },
     mainnet: {
       url: `https://eth-mainnet.alchemyapi.io/v2/${config.alchemy.mainnet.apiKey}`,
@@ -67,7 +58,7 @@ module.exports = {
     coinmarketcap: `${config.coinmarketcap.apiKey}`
   },
   preprocess: {
-    eachLine: removeConsoleLog((hre) => hre.network.name !== 'hardhat' && hre.network.name !== 'localhost'),
+    eachLine: removeConsoleLog((hre) => hre.network.name !== 'hardhat'),
   },
   etherscan: {
     apiKey: `${config.etherscan.apiKey}`
