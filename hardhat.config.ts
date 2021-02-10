93af2225af021a8caab0a18bbed63dada67e85e9
b0dhidharma <b0dhidharma@protonmail.com>
2021-02-10 18:42:47 -0300

    feat: deprecates .config file for dotenv usage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,9 @@
+import 'dotenv/config';
 import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-etherscan';
 import { removeConsoleLog } from 'hardhat-preprocessor';
 import 'hardhat-gas-reporter';
 import 'solidity-coverage';
-import config from './.config.json';
 import { utils } from 'ethers';
 
 module.exports = {
@@ -13,23 +13,21 @@ module.exports = {
       enabled: process.env.FORK ? true : false,
       accounts: [
         {
-          privateKey: config.accounts.mainnet.privateKey,
+          privateKey: process.env.MAINNET_PRIVATE_KEY,
           balance: utils.parseEther('1000').toString(),
         },
       ],
       forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/${config.alchemy.mainnet.apiKey}`,
+        url: process.env.MAINNET_HTTPS_URL,
       },
     },
     localMainnet: {
-      url: 'http://127.0.0.1:8545',
-      accounts: [config.accounts.mainnet.privateKey],
-      gasMultiplier: 1.1,
+      url: process.env.LOCAL_MAINNET_HTTPS_URL,
+      accounts: [process.env.LOCAL_MAINNET_PRIVATE_KEY],
     },
     mainnet: {
-      url: `https://eth-mainnet.alchemyapi.io/v2/${config.alchemy.mainnet.apiKey}`,
-      accounts: [config.accounts.mainnet.privateKey],
-      gasMultiplier: 1.1,
+      url: process.env.MAINNET_HTTPS_URL,
+      accounts: [process.env.MAINNET_PRIVATE_KEY],
       gasPrice: 'auto',
     },
   },
@@ -50,18 +48,17 @@ module.exports = {
     ],
   },
   mocha: {
-    timeout: 10 * 60 * 1000, // 10 minutes
+    timeout: process.env.MOCHA_TIMEOUT,
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS ? true : false,
-    currency: 'USD',
-    gasPrice: 200,
-    coinmarketcap: `${config.coinmarketcap.apiKey}`,
+    currency: process.env.COINMARKETCAP_DEFAULT_CURRENCY,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
   },
   preprocess: {
     eachLine: removeConsoleLog((hre) => hre.network.name !== 'hardhat'),
   },
   etherscan: {
-    apiKey: `${config.etherscan.apiKey}`,
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
