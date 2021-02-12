6574a459aeea9450552793a700a87eb80c54df73
b0dhidharma <b0dhidharma@protonmail.com>
2021-02-12 18:58:05 -0300

    feat: added new utils and tested deployment

--- test/utils/behaviours.ts
+++ test/utils/behaviours.ts
@@ -1,8 +1,12 @@
 import { expect } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import chai from 'chai';
-import { Contract, ContractFactory } from 'ethers';
-import { TransactionRequest } from '@ethersproject/abstract-provider';
+import { Contract, ContractFactory, ContractInterface, Signer } from 'ethers';
+import {
+  TransactionRequest,
+  TransactionResponse,
+} from '@ethersproject/abstract-provider';
+import { getStatic } from 'ethers/lib/utils';
 
 chai.use(chaiAsPromised);
 
@@ -26,6 +30,18 @@ const checkTxRevertedWithZeroAddress = async (
   });
 };
 
+const deployShouldRevertWithZeroAddress = async ({
+  contract,
+  args,
+}: {
+  contract: ContractFactory;
+  args: any[];
+}): Promise<void> => {
+  const deployContractTx = await contract.getDeployTransaction(...args);
+  const tx = contract.signer.sendTransaction(deployContractTx);
+  await checkTxRevertedWithZeroAddress(tx);
+};
+
 const txShouldRevertWithZeroAddress = async ({
   contract,
   func,
@@ -35,21 +51,96 @@ const txShouldRevertWithZeroAddress = async ({
   func: string;
   args: any[];
   tx?: Promise<TransactionRequest>;
-}) => {
+}): Promise<void> => {
   const tx = contract[func].apply(this, args);
   await checkTxRevertedWithZeroAddress(tx);
 };
 
-const deployShouldRevertWithZeroAddress = async ({
+const checkTxEmittedEvents = async ({
+  contract,
+  tx,
+  events,
+}: {
+  contract: Contract;
+  tx: Promise<TransactionRequest>;
+  events: [{ name: string; args: any[] }];
+}): Promise<void> => {
+  for (let i = 0; i < events.length; i++) {
+    await expect(tx)
+      .to.emit(contract, events[i].name)
+      .withArgs(...events[i].args);
+  }
+};
+
+const deployShouldSetVariablesAndEmitEvents = async ({
   contract,
   args,
+  settersGettersVariablesAndEvents,
 }: {
   contract: ContractFactory;
   args: any[];
-}) => {
+  settersGettersVariablesAndEvents: [
+    {
+      getterFunc: string;
+      variable: any;
+      eventEmitted: string;
+    }
+  ];
+}): Promise<void> => {
   const deployContractTx = await contract.getDeployTransaction(...args);
   const tx = contract.signer.sendTransaction(deployContractTx);
-  await checkTxRevertedWithZeroAddress(tx);
+  const address = getStatic<(tx: TransactionResponse) => string>(
+    contract.constructor,
+    'getContractAddress'
+  )(await tx);
+  const deployedContract = getStatic<
+    (
+      address: string,
+      contractInterface: ContractInterface,
+      signer?: Signer
+    ) => Contract
+  >(contract.constructor, 'getContract')(
+    address,
+    contract.interface,
+    contract.signer
+  );
+  await txShouldHaveSetVariablesAndEmitEvents({
+    contract: deployedContract,
+    tx,
+    settersGettersVariablesAndEvents,
+  });
+};
+
+const txShouldHaveSetVariablesAndEmitEvents = async ({
+  contract,
+  tx,
+  settersGettersVariablesAndEvents,
+}: {
+  contract: Contract;
+  tx: Promise<TransactionRequest>;
+  settersGettersVariablesAndEvents: [
+    {
+      getterFunc: string;
+      variable: any;
+      eventEmitted: string;
+    }
+  ];
+}): Promise<void> => {
+  for (let i = 0; i < settersGettersVariablesAndEvents.length; i++) {
+    await checkTxEmittedEvents({
+      contract,
+      tx,
+      events: [
+        {
+          name: settersGettersVariablesAndEvents[i].eventEmitted,
+          args: [settersGettersVariablesAndEvents[i].variable],
+        },
+      ],
+    });
+    expect(
+      await contract[settersGettersVariablesAndEvents[i].getterFunc].apply(this)
+    ).to.eq(settersGettersVariablesAndEvents[i].variable);
+  }
 };
 
 const txShouldSetVariableAndEmitEvent = async ({
@@ -64,16 +155,26 @@ const txShouldSetVariableAndEmitEvent = async ({
   getterFunc: string;
   variable: any;
   eventEmitted: string;
-}) => {
+}): Promise<void> => {
   expect(await contract[getterFunc].apply(this)).to.not.eq(variable);
-  await expect(contract[setterFunc].apply(this, [variable]))
-    .to.emit(contract, eventEmitted)
-    .withArgs(variable);
-  expect(await contract[getterFunc].apply(this)).to.eq(variable);
+  const tx = contract[setterFunc].apply(this, [variable]);
+  await txShouldHaveSetVariablesAndEmitEvents({
+    contract,
+    tx,
+    settersGettersVariablesAndEvents: [
+      {
+        getterFunc,
+        variable,
+        eventEmitted,
+      },
+    ],
+  });
 };
 
 export default {
-  txShouldRevertWithZeroAddress,
   deployShouldRevertWithZeroAddress,
+  txShouldRevertWithZeroAddress,
+  deployShouldSetVariablesAndEmitEvents,
+  txShouldHaveSetVariablesAndEmitEvents,
   txShouldSetVariableAndEmitEvent,
 };
