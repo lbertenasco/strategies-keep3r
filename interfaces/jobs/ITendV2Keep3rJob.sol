501b8f652715649808dcb2d0515693d70bc6ac05
Luciano Bertenasco <lbertenasco@gmail.com>
2021-02-17 13:42:39 -0300

    feat: reworks harvest and tend v2 jobs

--- interfaces/jobs/ITendV2Keep3rJob.sol
+++ interfaces/jobs/ITendV2Keep3rJob.sol
@@ -14,61 +14,24 @@ interface ITendV2Keep3rJob {
     // Getters
     function strategies() external view returns (address[] memory);
 
-    function harvestable(address _strategy) external view returns (bool);
-
-    function tendable(address _strategy) external view returns (bool);
-
-    // Keep3r actions
-    function harvest(address _strategy) external;
-
-    function tend(address _strategy) external;
-
-    // Governor keeper bypass
-    function forceHarvest(address _strategy) external;
-
-    function forceTend(address _strategy) external;
-
-    // Name of the Keep3r
-    function name() external pure returns (string memory);
-
-    event HarvestStrategyAdded(address _strategy, uint256 _requiredHarvest);
     event TendStrategyAdded(address _strategy, uint256 _requiredTend);
 
-    event HarvestStrategyModified(address _strategy, uint256 _requiredHarvest);
     event TendStrategyModified(address _strategy, uint256 _requiredTend);
 
-    event StrategyRemoved(address _strategy);
-    event HarvestStrategyRemoved(address _strategy);
     event TendStrategyRemoved(address _strategy);
 
     // Setters
-    function setHarvestCooldown(uint256 _harvestCooldown) external;
-
     function setTendCooldown(uint256 _tendCooldown) external;
 
     function setMaxCredits(uint256 _maxCredits) external;
 
-    function addStrategies(
-        address[] calldata _strategy,
-        uint256[] calldata _requiredHarvest,
-        uint256[] calldata _requiredTend
-    ) external;
+    function addStrategies(address[] calldata _strategy, uint256[] calldata _requiredTend) external;
 
-    function addStrategy(
-        address _strategy,
-        uint256 _requiredHarvest,
-        uint256 _requiredTend
-    ) external;
-
-    function updateRequiredHarvestAmount(address _strategy, uint256 _requiredHarvest) external;
+    function addStrategy(address _strategy, uint256 _requiredTend) external;
 
     function updateRequiredTendAmount(address _strategy, uint256 _requiredTend) external;
 
     function removeStrategy(address _strategy) external;
-
-    function removeHarvestStrategy(address _strategy) external;
-
-    function removeTendStrategy(address _strategy) external;
 
     // Governor work bypass
     function forceWork(address _strategy) external;
