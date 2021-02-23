1c5058204661a0a247c3e00d25421dda1ba98df1
Luciano Bertenasco <lbertenasco@gmail.com>
2021-02-23 12:06:09 -0300

    fix: removes unused imports

--- contracts/jobs/v2/TendV2Keep3rJob.sol
+++ contracts/jobs/v2/TendV2Keep3rJob.sol
@@ -2,16 +2,7 @@
 
 pragma solidity 0.6.12;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/EnumerableSet.sol";
-import "@lbertenasco/contract-utils/contracts/abstract/MachineryReady.sol";
-import "@lbertenasco/contract-utils/interfaces/keep3r/IKeep3rV1.sol";
-
 import "./V2Keep3rJob.sol";
-
-import "../../../interfaces/keep3r/IKeep3rV1Helper.sol";
-import "../../../interfaces/yearn/IBaseStrategy.sol";
-import "../../../interfaces/keep3r/IUniswapV2SlidingOracle.sol";
 
 contract TendV2Keep3rJob is V2Keep3rJob {
     constructor(
