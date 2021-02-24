1911b93835077f54300ace61f700409629697a36
steffel <2143646+steffenix@users.noreply.github.com>
2021-02-24 10:54:51 +0100

    feat: add LGT

--- contracts/sugar-mommy/Keep3rGasSaver.sol
+++ contracts/sugar-mommy/Keep3rGasSaver.sol
@@ -4,10 +4,17 @@ interface IFreeFromUpTo {
     function freeFromUpTo(address from, uint256 value) external returns (uint256 freed);
 }
 
+interface ILGT {
+    function freeFrom(uint256 amount, address owner) external returns (bool success);
+
+    function balanceOf(address account) external view returns (uint256);
+}
+
 abstract contract Keep3rGasSaver {
     using SafeMath for uint256;
     address public constant chi = address(0x0000000000004946c0e9F43F4Dee607b0eF1fA1c);
     address public constant gst2 = address(0x0000000000b3F879cb30FE243b4Dfee438691c04);
+    address public constant lgt = address(0x00000000007475142d6329FC42Dc9684c9bE6cD0);
 
     modifier saveOnGas(address _gas_token) {
         if (_gas_token == chi) {
@@ -44,6 +51,17 @@ abstract contract Keep3rGasSaver {
                 if (tokensToFree > 0) {
                     IFreeFromUpTo(gst2).freeFromUpTo(msg.sender, tokensToFree);
                 }
+            }
+        } else if (_gas_token == gst2) {
+            uint256 initialGas = gasleft();
+            _;
+            uint256 t = (initialGas - gasleft() + 19560) / 41717;
+            if (t > 0) {
+                uint256 balance = ILGT(lgt).balanceOf(msg.sender);
+                if (balance < t) {
+                    t = balance;
+                }
+                ILGT(lgt).freeFrom(t, msg.sender);
             }
         } else {
             _;
