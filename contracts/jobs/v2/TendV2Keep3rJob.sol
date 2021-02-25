550eaafd0a098cf09eb015ede427186a291c6956
b0dhidharma <78811746+b0dhidharma@users.noreply.github.com>
2021-02-25 16:07:03 -0300

    feat: deprecates work type on keep3r v2 job (#16)

--- contracts/jobs/v2/TendV2Keep3rJob.sol
+++ contracts/jobs/v2/TendV2Keep3rJob.sol
@@ -14,8 +14,16 @@ contract TendV2Keep3rJob is V2Keep3rJob {
         address _slidingOracle,
         uint256 _workCooldown,
         uint256 _maxCredits
-    ) public V2Keep3rJob(_mechanicsRegistry, _keep3rProxyJob, _v2Keeper, _keep3r, _keep3rHelper, _slidingOracle, _workCooldown, _maxCredits) {
-        // set workType as tend
-        workType = WorkType.tend;
+    ) public V2Keep3rJob(_mechanicsRegistry, _keep3rProxyJob, _v2Keeper, _keep3r, _keep3rHelper, _slidingOracle, _workCooldown, _maxCredits) {}
+
+    function _workable(address _strategy) internal view override returns (bool) {
+        if (!super._workable(_strategy)) return false;
+        (, uint256 _ethCallCost) = _getCallCosts(_strategy);
+        return IBaseStrategy(_strategy).tendTrigger(_ethCallCost);
+    }
+
+    function _work(address _strategy) internal override {
+        super._work(_strategy);
+        V2Keeper.tend(_strategy);
     }
 }
