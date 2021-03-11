ab2b593275e33b67274ec5e8ba7313471a6a2db0
Luciano Bertenasco <lbertenasco@gmail.com>
2021-03-11 16:16:09 -0300

    test: proxy keep3r job v2 workable (#18)
    Co-authored-by: b0dhidharma <b0dhidharma@protonmail.com>

--- contracts/mock/MechanicsRegistry.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.6.12;
-
-import "@lbertenasco/contract-utils/contracts/mechanics/MechanicsRegistry.sol";
-
-abstract contract MechanicsRegistryAbstract is MechanicsRegistry {}
