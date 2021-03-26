3a77b31401df62e9af2242e969ce4296e19e558f
Luciano Bertenasco <lbertenasco@gmail.com>
2021-03-26 15:49:24 -0300

    feat: deploy crv and vault v1 jobs

--- scripts/jobs/vaults/00-vault-keep3r-job-deploy.ts
+++ scripts/jobs/vaults/00-vault-keep3r-job-deploy.ts
@@ -1,6 +1,6 @@
 import { ContractFactory } from 'ethers';
 import { run, ethers } from 'hardhat';
-import { e18, ZERO_ADDRESS } from '../../../utils/web3-utils';
+import { gwei, ZERO_ADDRESS } from '../../../utils/web3-utils';
 import config from '../../../.config.json';
 const mainnetContracts = config.contracts.mainnet;
 const mechanicsContracts = mainnetContracts.mechanics;
@@ -10,52 +10,49 @@ const prompt = new Confirm('Do you wish to deploy crv keep3r contract?');
 
 async function main() {
   await run('compile');
-  const CrvStrategyKeep3rJob: ContractFactory = await ethers.getContractFactory(
-    'CrvStrategyKeep3rJob'
+  const VaultKeep3rJob: ContractFactory = await ethers.getContractFactory(
+    'VaultKeep3rJob'
   );
-  await promptAndSubmit(CrvStrategyKeep3rJob);
+  await promptAndSubmit(VaultKeep3rJob);
 }
 
 function promptAndSubmit(
-  CrvStrategyKeep3rJob: ContractFactory
+  VaultKeep3rJob: ContractFactory
 ): Promise<void | Error> {
   return new Promise(async (resolve, reject) => {
     const [owner] = await ethers.getSigners();
     console.log('using address:', owner.address);
     prompt.run().then(async (answer: any) => {
       if (answer) {
-        console.time('CrvStrategyKeep3rJob deployed');
+        console.time('VaultKeep3rJob deployed');
         try {
           console.log(
             mechanicsContracts.registry,
             mainnetContracts.keep3r.address,
             ZERO_ADDRESS,
-            e18.mul(50).toString(), // 50 KP3R required
+            0, // 0 KP3R required
             0,
             0,
             true,
-            24 * 60 * 60, // 1 day maxHarvestPeriod,
-            30 * 60 // 30 minutes harvestCooldown
+            6 * 60 * 60, // 6 hours earnCooldown
+            gwei.mul(185).toString() // 185 gwei maxGasPrice
           );
-          const crvStrategyKeep3rJob = await CrvStrategyKeep3rJob.deploy(
+          const vaultKeep3rJob = await VaultKeep3rJob.deploy(
             mechanicsContracts.registry,
             mainnetContracts.keep3r.address,
             ZERO_ADDRESS,
-            e18.mul(50), // 50 KP3R required
+            0, // 0 KP3R required
             0,
             0,
             true,
-            24 * 60 * 60, // 1 day maxHarvestPeriod,
-            30 * 60 // 30 minutes harvestCooldown
+            6 * 60 * 60, // 6 hours earnCooldown
+            gwei.mul(185) // 185 gwei maxGasPrice
           );
-          console.timeEnd('CrvStrategyKeep3rJob deployed');
+          console.timeEnd('VaultKeep3rJob deployed');
+          console.log('VaultKeep3rJob address:', vaultKeep3rJob.address);
           console.log(
-            'CrvStrategyKeep3rJob address:',
-            crvStrategyKeep3rJob.address
-          );
-          console.log(
-            'PLEASE: change .config.json & example.config.json proxyJobs.crvStrategyKeep3rJob address to:',
-            crvStrategyKeep3rJob.address
+            'PLEASE: change .config.json & example.config.json proxyJobs.vaultKeep3rJob address to:',
+            vaultKeep3rJob.address
           );
           resolve();
         } catch (err) {
