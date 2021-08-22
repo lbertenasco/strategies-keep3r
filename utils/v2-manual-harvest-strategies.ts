35ad7c020fa1d1db141fb4fb30b1b248384e78ca
dudesahn <23222916+dudesahn@users.noreply.github.com>
2021-08-22 15:35:49 -0400

    feat: update manual job file

--- utils/v2-manual-harvest-strategies.ts
+++ utils/v2-manual-harvest-strategies.ts
@@ -1,49 +1,49 @@
 import { e18 } from './web3-utils';
 
 export const manualHarvestStrategies = [
-  /* BTC */
-  {
-    name: 'convex_hcrv',
-    address: '0x7Ed0d52C5944C7BF92feDC87FEC49D474ee133ce',
-    maxReportDelay: 60 * 60 * 24 * 4, // 4 days
-    amount: e18.mul(5),
-  },
-  {
-    name: 'convex_bbtc',
-    address: '0xE9ac8D34C546CBfdAD98F9a4546Db5fE08D01bF2',
-    maxReportDelay: 60 * 60 * 24 * 4, // 4 days
-    amount: e18.mul(5),
-  },
-  {
-    name: 'convex_obtc',
-    address: '0xDb2D3F149270630382D4E6B4dbCd47e665D78D76',
-    maxReportDelay: 60 * 60 * 24 * 6, // 6 days
-    amount: e18.mul(5),
-  },
-  {
-    name: 'convex_pbtc',
-    address: '0x7b5cb4694b0A299ED2F65db7d87B286461549e84',
-    maxReportDelay: 60 * 60 * 24 * 1.25, // 30 hours
-    amount: e18.mul(5),
-  },
-  {
-    name: 'convex_sbtc',
-    address: '0x7aB4DB515bf258A88Bb14f3685769a0f70B8778f',
-    maxReportDelay: 60 * 60 * 24 * 6.73, // 6.73 days
-    amount: e18.mul(5),
-  },
-  {
-    name: 'convex_tbtc',
-    address: '0x07fb6A53185E2F095253099A47F34CD410eB2A89',
-    maxReportDelay: 60 * 60 * 24 * 5, // 5 days
-    amount: e18.mul(5),
-  },
-  {
-    name: 'convex_renbtc',
-    address: '0x7799F476522Ebe259fc525C1A21E84f7Dd551955',
-    maxReportDelay: 60 * 60 * 24 * 6.93, // 6.93 days
-    amount: e18.mul(5),
-  },
+  //   BTC */ all of these strategies have been moved to stealth job
+  //   {
+  //     name: 'convex_hcrv',
+  //     address: '0x7Ed0d52C5944C7BF92feDC87FEC49D474ee133ce',
+  //     maxReportDelay: 60 * 60 * 24 * 4, // 4 days
+  //     amount: e18.mul(5),
+  //   },
+  //   {
+  //     name: 'convex_bbtc',
+  //     address: '0xE9ac8D34C546CBfdAD98F9a4546Db5fE08D01bF2',
+  //     maxReportDelay: 60 * 60 * 24 * 4, // 4 days
+  //     amount: e18.mul(5),
+  //   },
+  //   {
+  //     name: 'convex_obtc',
+  //     address: '0xDb2D3F149270630382D4E6B4dbCd47e665D78D76',
+  //     maxReportDelay: 60 * 60 * 24 * 6, // 6 days
+  //     amount: e18.mul(5),
+  //   },
+  //   {
+  //     name: 'convex_pbtc',
+  //     address: '0x7b5cb4694b0A299ED2F65db7d87B286461549e84',
+  //     maxReportDelay: 60 * 60 * 24 * 1.25, // 30 hours
+  //     amount: e18.mul(5),
+  //   },
+  //   {
+  //     name: 'convex_sbtc',
+  //     address: '0x7aB4DB515bf258A88Bb14f3685769a0f70B8778f',
+  //     maxReportDelay: 60 * 60 * 24 * 6.73, // 6.73 days
+  //     amount: e18.mul(5),
+  //   },
+  //   {
+  //     name: 'convex_tbtc',
+  //     address: '0x07fb6A53185E2F095253099A47F34CD410eB2A89',
+  //     maxReportDelay: 60 * 60 * 24 * 5, // 5 days
+  //     amount: e18.mul(5),
+  //   },
+  //   {
+  //     name: 'convex_renbtc',
+  //     address: '0x7799F476522Ebe259fc525C1A21E84f7Dd551955',
+  //     maxReportDelay: 60 * 60 * 24 * 6.93, // 6.93 days
+  //     amount: e18.mul(5),
+  //   },
   // /* ETH */ Moved to stealth job
   // {
   //   name: 'convex_ankr',
@@ -77,151 +77,151 @@ export const manualHarvestStrategies = [
   //   amount: e18.mul(250),
   // },
   /* STABLECOINS */
-  {
-    name: 'convex_usdp',
-    address: '0xfb0702469A1a0440E87C06605461E8660FD0F43d',
-    maxReportDelay: 60 * 60 * 24 * 6, // 6 days
-    amount: e18.mul(200000),
-  },
-  {
-    name: 'convex_usdn',
-    address: '0x8e87e65Cb28c069550012f92d5470dB6EB6897c0',
-    maxReportDelay: 60 * 60 * 8, // 8 hours
-    amount: e18.mul(2000000),
-  },
-  {
-    name: 'convex_comp',
-    address: '0x2b0b941d98848d6c9C729d944E3B1BD9C00A5529',
-    maxReportDelay: 60 * 60 * 24 * 5, // 5 days
-    amount: e18.mul(200000),
-  },
-  {
-    name: 'convex_ybusd',
-    address: '0x3cA0B4d7eedE71061B0bAdb4F0E86E99b0FEa613',
-    maxReportDelay: 60 * 60 * 24 * 6.75, // 6.75 days
-    amount: e18.mul(200000),
-  },
-  {
-    name: 'convex_susd',
-    address: '0xFA773b91b59B0895877c769000b9824b46b13a20',
-    maxReportDelay: 60 * 60 * 24 * 6, // 6 days
-    amount: e18.mul(200000),
-  },
-  {
-    name: 'convex_3pool',
-    address: '0xeC088B98e71Ba5FFAf520c2f6A6F0153f1bf494B',
-    maxReportDelay: 60 * 60 * 24 * 6.9, // 6.9 days
-    amount: e18.mul(200000),
-  },
-  {
-    name: 'convex_yusd',
-    address: '0xA5189cb0149761A8346D64E384924b2394dFa595',
-    maxReportDelay: 60 * 60 * 24 * 6.8, // 6.8 days
-    amount: e18.mul(200000),
-  },
-  {
-    name: 'convex_gusd',
-    address: '0x2D42CFdC6a1B03490892AdF7DC6c62AA7228E5D6',
-    maxReportDelay: 60 * 60 * 24 * 6, // 6 days
-    amount: e18.mul(200000),
-  },
-  {
-    name: 'convex_ust',
-    address: '0x0921E388e86bbE0356e37413F946ccE47EDd294D',
-    maxReportDelay: 60 * 60 * 24 * 6.75, // 6.75 days
-    amount: e18.mul(200000),
-  },
-  {
-    name: 'convex_husd',
-    address: '0xdC929e76081a78e5c32552C2e79D29ECab3F6755',
-    maxReportDelay: 60 * 60 * 24 * 7, // 7 days
-    amount: e18.mul(200000),
-  },
-  {
-    name: 'convex_dusd',
-    address: '0x33d7E0Fa2c7Db85Ef3AbC1C44e07E0b5cB2E4C14',
-    maxReportDelay: 60 * 60 * 24 * 6.3, // 6.3 days
-    amount: e18.mul(200000),
-  },
+  //   {
+  //     name: 'convex_usdp',
+  //     address: '0xfb0702469A1a0440E87C06605461E8660FD0F43d',
+  //     maxReportDelay: 60 * 60 * 24 * 6, // 6 days
+  //     amount: e18.mul(200000),
+  //   },
+  //   {
+  //     name: 'convex_usdn',
+  //     address: '0x8e87e65Cb28c069550012f92d5470dB6EB6897c0',
+  //     maxReportDelay: 60 * 60 * 8, // 8 hours
+  //     amount: e18.mul(2000000),
+  //   },
+  //   {
+  //     name: 'convex_comp',
+  //     address: '0x2b0b941d98848d6c9C729d944E3B1BD9C00A5529',
+  //     maxReportDelay: 60 * 60 * 24 * 5, // 5 days
+  //     amount: e18.mul(200000),
+  //   },
+  //   {
+  //     name: 'convex_ybusd',
+  //     address: '0x3cA0B4d7eedE71061B0bAdb4F0E86E99b0FEa613',
+  //     maxReportDelay: 60 * 60 * 24 * 6.75, // 6.75 days
+  //     amount: e18.mul(200000),
+  //   },
+  //   {
+  //     name: 'convex_susd',
+  //     address: '0xFA773b91b59B0895877c769000b9824b46b13a20',
+  //     maxReportDelay: 60 * 60 * 24 * 6, // 6 days
+  //     amount: e18.mul(200000),
+  //   },
+  //   {
+  //     name: 'convex_3pool',
+  //     address: '0xeC088B98e71Ba5FFAf520c2f6A6F0153f1bf494B',
+  //     maxReportDelay: 60 * 60 * 24 * 6.9, // 6.9 days
+  //     amount: e18.mul(200000),
+  //   },
+  //   {
+  //     name: 'convex_yusd',
+  //     address: '0xA5189cb0149761A8346D64E384924b2394dFa595',
+  //     maxReportDelay: 60 * 60 * 24 * 6.8, // 6.8 days
+  //     amount: e18.mul(200000),
+  //   },
+  //   {
+  //     name: 'convex_gusd',
+  //     address: '0x2D42CFdC6a1B03490892AdF7DC6c62AA7228E5D6',
+  //     maxReportDelay: 60 * 60 * 24 * 6, // 6 days
+  //     amount: e18.mul(200000),
+  //   },
+  //   {
+  //     name: 'convex_ust',
+  //     address: '0x0921E388e86bbE0356e37413F946ccE47EDd294D',
+  //     maxReportDelay: 60 * 60 * 24 * 6.75, // 6.75 days
+  //     amount: e18.mul(200000),
+  //   },
+  //   {
+  //     name: 'convex_husd',
+  //     address: '0xdC929e76081a78e5c32552C2e79D29ECab3F6755',
+  //     maxReportDelay: 60 * 60 * 24 * 7, // 7 days
+  //     amount: e18.mul(200000),
+  //   },
+  //   {
+  //     name: 'convex_dusd',
+  //     address: '0x33d7E0Fa2c7Db85Ef3AbC1C44e07E0b5cB2E4C14',
+  //     maxReportDelay: 60 * 60 * 24 * 6.3, // 6.3 days
+  //     amount: e18.mul(200000),
+  //   },
   // { // deprecated
   //   name: 'convex_musd',
   //   address: '0x75be6ABC02a010559Ed5c7b0Eab94abD2B783b65',
   //   maxReportDelay: 60 * 60 * 24 * 6.75, // 6.75 days
   //   amount: e18.mul(100000),
   // },
-  {
-    name: 'convex_aave',
-    address: '0xAC4AE0B06C913dF4608dB60E2571a8e91b74C619',
-    maxReportDelay: 60 * 60 * 24 * 6.7, // 6.7 days
-    amount: e18.mul(200000),
-  },
-  {
-    name: 'convex_saave',
-    address: '0xF5636591256195414f25d19034B70A4742Fc2A2e',
-    maxReportDelay: 60 * 60 * 24 * 5, // 5 days
-    amount: e18.mul(300000),
-  },
-  {
-    name: 'convex_lusd',
-    address: '0x789685963DF287337759A9FaB65d8c645a3B4cba',
-    maxReportDelay: 60 * 60 * 24 * 1.2, // 1.2 day
-    amount: e18.mul(300000),
-  },
-  {
-    name: 'convex_frax',
-    address: '0x8c312B63Bc4000f61E1C4df4868A3A1f09b31A73',
-    maxReportDelay: 60 * 60 * 24 * 2, // 2 days
-    amount: e18.mul(500000),
-  },
-  {
-    // deprecated
-    name: 'convex_busd',
-    address: '0xA44F947e51Ec6456A1d786F82ea5865F87Da9C30',
-    maxReportDelay: 60 * 60 * 24 * 6.94, // 6.94 days
-    amount: e18.mul(100000),
-  },
-  {
-    name: 'convex_tusd',
-    address: '0x270101459e9A38Db38Ba4Cb8718FfA31953A9Af3',
-    maxReportDelay: 60 * 60 * 24 * 7, // 7 days
-    amount: e18.mul(200000),
-  },
-  {
-    name: 'convex_alusd',
-    address: '0xf8Fb278DeeaF30Ff3F6326d928A61eA8b9397d16',
-    maxReportDelay: 60 * 60 * 24 * 6, // 6 days
-    amount: e18.mul(200000),
-  },
-  {
-    name: 'convex_ironbank',
-    address: '0xf0aAba6bb8E6bAE83Ea984BC4b7dcf0fF54a8FEF',
-    maxReportDelay: 60 * 60 * 14, // 14 hours
-    amount: e18.mul(1500000),
-  },
-  {
-    name: 'curve_ironbank',
-    address: '0xf0aAba6bb8E6bAE83Ea984BC4b7dcf0fF54a8FEF',
-    maxReportDelay: 60 * 60 * 22, // 22 hours
-    amount: e18.mul(1500000),
-  },
-  {
-    name: 'convex_seth',
-    address: '0x22D07F42Cf4D077E765560ff6A741eF8E851091c',
-    maxReportDelay: 60 * 60 * 90, // 90 hours
-    amount: e18.mul(200),
-  },
-  {
-    name: 'convex_steth',
-    address: '0x6C0496fC55Eb4089f1Cf91A4344a2D56fAcE51e3',
-    maxReportDelay: 60 * 60 * 8, // 8 hours
-    amount: e18.mul(300),
-  },
-  {
-    name: 'convex_3crypto',
-    address: '0x2055CFD5CDbc90c60A202A1AC3DDfB71AeC1cE98',
-    maxReportDelay: 60 * 60 * 40, // 40 hours
-    amount: e18.mul(500),
-  },
+  //   {
+  //     name: 'convex_aave',
+  //     address: '0xAC4AE0B06C913dF4608dB60E2571a8e91b74C619',
+  //     maxReportDelay: 60 * 60 * 24 * 6.7, // 6.7 days
+  //     amount: e18.mul(200000),
+  //   },
+  //   {
+  //     name: 'convex_saave',
+  //     address: '0xF5636591256195414f25d19034B70A4742Fc2A2e',
+  //     maxReportDelay: 60 * 60 * 24 * 5, // 5 days
+  //     amount: e18.mul(300000),
+  //   },
+  //   {
+  //     name: 'convex_lusd',
+  //     address: '0x789685963DF287337759A9FaB65d8c645a3B4cba',
+  //     maxReportDelay: 60 * 60 * 24 * 1.2, // 1.2 day
+  //     amount: e18.mul(300000),
+  //   },
+  //   {
+  //     name: 'convex_frax',
+  //     address: '0x8c312B63Bc4000f61E1C4df4868A3A1f09b31A73',
+  //     maxReportDelay: 60 * 60 * 24 * 2, // 2 days
+  //     amount: e18.mul(500000),
+  //   },
+  //   {
+  //     // deprecated
+  //     name: 'convex_busd',
+  //     address: '0xA44F947e51Ec6456A1d786F82ea5865F87Da9C30',
+  //     maxReportDelay: 60 * 60 * 24 * 6.94, // 6.94 days
+  //     amount: e18.mul(100000),
+  //   },
+  //   {
+  //     name: 'convex_tusd',
+  //     address: '0x270101459e9A38Db38Ba4Cb8718FfA31953A9Af3',
+  //     maxReportDelay: 60 * 60 * 24 * 7, // 7 days
+  //     amount: e18.mul(200000),
+  //   },
+  //   {
+  //     name: 'convex_alusd',
+  //     address: '0xf8Fb278DeeaF30Ff3F6326d928A61eA8b9397d16',
+  //     maxReportDelay: 60 * 60 * 24 * 6, // 6 days
+  //     amount: e18.mul(200000),
+  //   },
+  //   {
+  //     name: 'convex_ironbank',
+  //     address: '0xf0aAba6bb8E6bAE83Ea984BC4b7dcf0fF54a8FEF',
+  //     maxReportDelay: 60 * 60 * 14, // 14 hours
+  //     amount: e18.mul(1500000),
+  //   },
+  //   {
+  //     name: 'curve_ironbank',
+  //     address: '0x6D892A5585F45f3C45A238C717AA0a228aE69e2C',
+  //     maxReportDelay: 60 * 60 * 22, // 22 hours
+  //     amount: e18.mul(1500000),
+  //   },
+  //   {
+  //     name: 'convex_seth',
+  //     address: '0x22D07F42Cf4D077E765560ff6A741eF8E851091c',
+  //     maxReportDelay: 60 * 60 * 90, // 90 hours
+  //     amount: e18.mul(200),
+  //   },
+  //   {
+  //     name: 'convex_steth',
+  //     address: '0x6C0496fC55Eb4089f1Cf91A4344a2D56fAcE51e3',
+  //     maxReportDelay: 60 * 60 * 8, // 8 hours
+  //     amount: e18.mul(300),
+  //   },
+  //   {
+  //     name: 'convex_3crypto',
+  //     address: '0x2055CFD5CDbc90c60A202A1AC3DDfB71AeC1cE98',
+  //     maxReportDelay: 60 * 60 * 40, // 40 hours
+  //     amount: e18.mul(500),
+  //   },
   {
     name: 'idle_rai_yield',
     address: '0x5d411d2cde10e138d68517c42be2808c90c22026',
